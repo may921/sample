ae7bdb5d9e53da9543ebfa116496f95ecc97308b
may921 <mayzzv@yahoo.co.jp>
2020-08-06 00:49:34 +0900

    検索フォーム更新

--- serch.php
+++ serch.php
@@ -1,60 +1,27 @@
 <?php
 
-
-//DB接続情報
+//検索されたときだけ処理をする
 if($_POST){
-    $id = null;
-    $name = $_POST["name"];
-	$contents = $_POST["contents"];
-	
-	//エラーチェック
-	if(!$name) {
-		$errors[] .= "名前を入力してください";
-	}
-	if(!$contents){
-        $errors[] .= "投稿内容を入力してください";
-	}
-	
-	if(!$errors){
-		date_default_timezone_set('Asia/Tokyo');
-		$created_at = date("Y-m-d H:i:s");
-		
-		//DB接続情報を設定します。
-		$pdo = new PDO(
-			"mysql:dbname=sample;host=localhost","root","Passw0rd",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
-		);
-		//ここで「DB接続NG」だった場合、接続情報に誤りがあります。
-		if ($pdo) {
-			echo "DB接続OK";
-		} else {
-        echo "DB接続NG";
-		}
-		//SQLを実行。
-		$regist = $pdo->prepare("INSERT INTO post(id, name, contents, created_at) VALUES (:id,:name,:contents,:created_at)");
-		$regist->bindParam(":id", $id);
-		$regist->bindParam(":name", $name);
-		$regist->bindParam(":contents", $contents);
-		$regist->bindParam(":created_at", $created_at);
-		$regist->execute();
-		//ここで「登録失敗」だった場合、SQL文に誤りがあります。
-		if ($regist) {
-			echo "登録成功";
-		} else {
-			echo "登録失敗";
-		}
-	}
+
+	$serchText = $_POST["serchText"];
+
+
+	//DB接続情報を設定します。
+	$pdo = new PDO(
+		"mysql:dbname=sample;host=localhost","root","Passw0rd",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
+	);
+
+	//SQLを実行。
+	//$regist = $pdo->prepare("SELECT * FROM post");
+	$regist = $pdo->prepare("SELECT * FROM post order by created_at DESC limit 20");
+
+	//SQL文を実行して、結果を$serchResultに代入する。
+	$serchResult = $pdo->query("SELECT * FROM post WHERE contents LIKE '%".$_POST["serchText"]."%'"); 
+	$regist->execute();
+
+
+
 }
-$pdo = new PDO(
-	"mysql:dbname=sample;host=localhost","root","Passw0rd",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
-);
-
-
-//SQL
-//$regist = $pdo->prepare("SELECT * FROM post");
-$regist = $pdo->prepare("SELECT * FROM post order by created_at DESC limit 20");
-$regist->execute();
-
-
 
 
 ?>
@@ -64,18 +31,16 @@ $regist->execute();
 <title>投稿内容検索</title>
 <h1>投稿内容検索</h1>
 <section>
-	<h2>新規投稿</h2>
-	<div id="error"><?php foreach($errors as $error){echo $error.'<br>';}?></div>
-	<form action="index.php" method="post">
-	名前：<input type="text" name="name" value=""><br>
-	投稿内容:<input type="text" name="contents" value=""><br>
-	<button type="submit">投稿</button>
+	<h2>検索内容</h2>
+	<form action="serch.php" method="post">
+	<input type="text" name="serchText" value="<?php echo $_POST['serchText']?>"><br>
+	<button type="submit">検索</button>
 	</form>
 </section>
 
 
 <section>
-	<h2>投稿内容一覧</h2>
+	<h2>検索内容一覧</h2>
 		<?php foreach($regist as $loop):?>
 			<div>No:<?php echo $loop['id']?></div>
 			<div>名前:<?php echo $loop['name']?></div>
